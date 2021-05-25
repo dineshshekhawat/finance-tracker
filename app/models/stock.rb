f665624859e2c145ecc5b567826e9e3c084e2241
Dinesh <shekhawat.dinesh@gmail.com>
2021-05-25 17:55:19 +0530

    Add user stock association

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,6 +4,10 @@ class Stock < ApplicationRecord
 
     @@API_KEY = ENV["IEX_CLOUD_API_KEY"]
 
+    def self.find_by_ticker(ticker_symbol)
+        where(ticker: ticker_symbol).first
+    end
+
     def self.new_from_lookup(ticker_symbol)
         begin
             StockQuote::Stock.new(api_key: @@API_KEY)
