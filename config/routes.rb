5e007f1e2566146ebf426c46421c5908b58c1030
Dinesh <shekhawat.dinesh@gmail.com>
2021-05-29 17:41:41 +0530

    Add Friends list UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "user/registrations" }
+  resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
 
   root 'welcome#index'
   
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
   get 'my_friends', to: "users#my_friends"
+  get 'search_firends', to: "users#search"
   
-  resources :user_stocks, only: [:create, :destroy]
 end
